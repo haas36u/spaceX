54bef4cb747acfe8aa269d76b83743291d06c112
vincent monjaret <vmonjaret@gmail.com>
2018-05-15 15:29:24 +0200

    #5: fix typo in files
    Signed-off-by: vincent monjaret <vmonjaret@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {SpacexApiService} from "./services/spacex-api-service.service";
+import {SpacexApiService} from "./services/spacex-api.service";
 
 @Component({
 	selector: 'app-root',
