58919bd127e673be24acf03b0e39de6bbe4626a3
Myriam <haasmyriam8@gmail.com>
2018-05-15 14:30:48 +0200

    Begin basic design and create some routes

--- src/app/services/spacex-api-service.service.ts
+++ src/app/services/spacex-api-service.service.ts
@@ -7,85 +7,85 @@ import {LaunchFilter} from "../models/launchFilter";
 import { Launch } from '../models/launch';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class SpacexApiService {
 
-  private baseUrl = 'https://api.spacexdata.com/v2';
+	private baseUrl = 'https://api.spacexdata.com/v2';
 
-  constructor(private http: HttpClient) {
-  }
+	constructor(private http: HttpClient) {
+	}
 
-  getCompanyInfo(): Observable<Company> {
-    const requestEndpoint = `${this.baseUrl}/info`;
-    return this.http.get<Company>(requestEndpoint)
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
+	getCompanyInfo(): Observable<Company> {
+		const requestEndpoint = `${this.baseUrl}/info`;
+		return this.http.get<Company>(requestEndpoint)
+		.pipe(
+			catchError(this.handleError)
+		);
+	}
 
-  getLastLaunch(filter?: LaunchFilter): Observable<Launch> {
-    const params = this.filtersToHttpParams(filter);
+	getLastLaunch(filter?: LaunchFilter): Observable<Launch> {
+		const params = this.filtersToHttpParams(filter);
 
-    const requestEndpoint = `${this.baseUrl}/launches/latest`;
-    return this.http.get<Launch>(requestEndpoint, {params})
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
+		const requestEndpoint = `${this.baseUrl}/launches/latest`;
+		return this.http.get<Launch>(requestEndpoint, {params})
+		.pipe(
+			catchError(this.handleError)
+		);
+	}
 
-  getPastLaunches(filter?: LaunchFilter): Observable<Launch> {
-    const params = this.filtersToHttpParams(filter);
+	getPastLaunches(filter?: LaunchFilter): Observable<Launch> {
+		const params = this.filtersToHttpParams(filter);
 
-    const requestEndpoint = `${this.baseUrl}/launches`;
-    return this.http.get<Launch>(requestEndpoint, {params})
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
+		const requestEndpoint = `${this.baseUrl}/launches`;
+		return this.http.get<Launch>(requestEndpoint, {params})
+		.pipe(
+			catchError(this.handleError)
+		);
+	}
 
-  getUpcomingLaunches(filter?: LaunchFilter): Observable<Launch> {
-    const params = this.filtersToHttpParams(filter);
+	getUpcomingLaunches(filter?: LaunchFilter): Observable<Launch> {
+		const params = this.filtersToHttpParams(filter);
 
-    const requestEndpoint = `${this.baseUrl}/launches/upcoming`;
-    return this.http.get<Launch>(requestEndpoint, {params})
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
+		const requestEndpoint = `${this.baseUrl}/launches/upcoming`;
+		return this.http.get<Launch>(requestEndpoint, {params})
+		.pipe(
+			catchError(this.handleError)
+		);
+	}
 
-  getAllLaunches(filter?: LaunchFilter): Observable<Launch> {
-    const params = this.filtersToHttpParams(filter);
+	getAllLaunches(filter?: LaunchFilter): Observable<Launch> {
+		const params = this.filtersToHttpParams(filter);
 
-    const requestEndpoint = `${this.baseUrl}/launches/all`;
-    return this.http.get<Launch>(requestEndpoint, {params})
-      .pipe(
-        catchError(this.handleError)
-      );
-  }
+		const requestEndpoint = `${this.baseUrl}/launches/all`;
+		return this.http.get<Launch>(requestEndpoint, {params})
+		.pipe(
+			catchError(this.handleError)
+		);
+	}
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
+	private handleError(error: HttpErrorResponse) {
+		if (error.error instanceof ErrorEvent) {
+		// A client-side or network error occurred. Handle it accordingly.
+		console.error('An error occurred:', error.error.message);
+		} else {
+		// The backend returned an unsuccessful response code.
+		// The response body may contain clues as to what went wrong,
+		console.error(
+			`Backend returned code ${error.status}, ` +
+			`body was: ${error.error}`);
+		}
+		// return an observable with a user-facing error message
+		return throwError(
+		'Something bad happened; please try again later.');
+	};
 
-  private filtersToHttpParams(filter: LaunchFilter): HttpParams {
-    let params = new HttpParams();
-    for (let key in filter) {
-      params = params.append(key, filter[key]);
-    }
+	private filtersToHttpParams(filter: LaunchFilter): HttpParams {
+		let params = new HttpParams();
+		for (let key in filter) {
+		params = params.append(key, filter[key]);
+		}
 
-    return params;
-  }
+		return params;
+	}
 }
