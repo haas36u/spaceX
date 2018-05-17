3ea67b9fba3c8861aaaa102a61236caaa543d536
vincent monjaret <vmonjaret@gmail.com>
2018-05-17 12:53:44 +0200

    added details
    Signed-off-by: vincent monjaret <vmonjaret@gmail.com>

--- src/app/components/capsule/capsule-details/capsule-details.component.ts
+++ src/app/components/capsule/capsule-details/capsule-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {SpacexApiService} from "../../../services/spacex-api.service";
+import {Capsule} from "../../../models/capsule";
 
 @Component({
   selector: 'app-capsule-details',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapsuleDetailsComponent implements OnInit {
 
-  constructor() { }
+  public capsule: Capsule;
+
+  constructor(private route: ActivatedRoute, private spacexApi: SpacexApiService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.spacexApi.getCapsule(params['id']).subscribe(capsule => this.capsule = capsule);
+    });
   }
+
 
 }
